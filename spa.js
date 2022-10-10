8ac71f5cc8702faee146c06c1fd2fde4be9089f3
justnotiyann <muhammadfitrian0712@gmail.com>
2022-10-10 20:07:36 +0700

    add scss

--- spa.js
+++ spa.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
   $("#home_selector").click(() => {
-    $(".contact").slideUp(500);
-    $(".tugas_tugas").slideUp(500)
+    $(".contact").sllide(500);
+    $(".tugas_tugas").slideUp(500);
     $(".home").toggle(500);
   });
   $("#tugas").click(() => {
     $(".contact").slideUp(500);
-    $(".home").slideUp(500)
+    $(".home").slideUp(500);
+    $("#home").toggle(500);
     $(".tugas_tugas").toggle(500);
   });
   $("#contact_selector").click(() => {
-    $(".home").slideUp(500)
+    $(".home").slideUp(500);
     $(".tugas_tugas").slideUp(500);
+    $("#home").toggle(500);
     $(".contact").toggle(500);
   });
-
 });
